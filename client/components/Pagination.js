5bf727f556f902f1b6f916b095a199d7027ace75
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-22 13:33:03 +0000

    align pagination

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -14,13 +14,13 @@ class Pagination extends Component {
                         <p>Still in development</p>
                         <nav role="navigation">
                             <ul className="pagination no-space move-buttons">
-                                <li className="button"><a href="#0">Prev</a></li>
-                                <li><a href="#0">1</a></li>
-                                <li><a href="#0">2</a></li>
-                                <li><a className="current" href="#0">3</a></li>
-                                <li><a href="#0">4</a></li>
+                                <li><a className="button" href="#0">Prev</a></li>
+                                <li><a className="button-pagination" href="#0">1</a></li>
+                                <li><a className="button-pagination" href="#0">2</a></li>
+                                <li><a className="button-pagination current" href="#0">3</a></li>
+                                <li><a className="button-pagination" href="#0">4</a></li>
                                 <li><span>...</span></li>
-                                <li><a href="#0">20</a></li>
+                                <li><a className="button-pagination" href="#0">20</a></li>
                                 <li className="button"><a href="#0">Next</a></li>
                             </ul>
                         </nav>
