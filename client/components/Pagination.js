fa268e893f3ce78c60a67005bfa7f76ac829b900
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-22 13:48:24 +0000

    fix pagination and apply selected class

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -11,11 +11,10 @@ class Pagination extends Component {
                 <div className="wrapper">
                     <main>
                         <h1>Pagination</h1>
-                        <p>Still in development</p>
                         <nav role="navigation">
-                            <ul className="pagination no-space move-buttons">
+                            <ul className="pagination">
                                 <li><a className="button" href="#0">Prev</a></li>
-                                <li><a className="button button-pagination" href="#0">1</a></li>
+                                <li><a className="button button-pagination selected" href="#0">1</a></li>
                                 <li><a className="button button-pagination" href="#0">2</a></li>
                                 <li><a className="button button-pagination current" href="#0">3</a></li>
                                 <li><a className="button button-pagination" href="#0">4</a></li>
