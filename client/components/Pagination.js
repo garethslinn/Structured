75b59158c685b1d040230e06ca4a953115ab3bd4
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-22 16:20:55 +0000

    apply html

--- client/components/Pagination.js
+++ client/components/Pagination.js
@@ -13,16 +13,57 @@ class Pagination extends Component {
                         <h1>Pagination</h1>
                         <nav role="navigation">
                             <ul className="pagination">
-                                <li><a className="button" href="#">Prev</a></li>
+                                <li><a className="button" href="#">
+                                    <i class="fa fa-angle-double-left" aria-hidden="true">&nbsp;</i>
+                                    Prev</a>
+                                </li>
                                 <li><a className="button button-pagination selected" href="#">1</a></li>
                                 <li><a className="button button-pagination" href="#">2</a></li>
-                                <li><a className="button button-pagination current" href="#">3</a></li>
+                                <li><a className="button button-pagination" href="#">3</a></li>
                                 <li><a className="button button-pagination" href="#">4</a></li>
                                 <li><span>...</span></li>
                                 <li><a className="button button-pagination" href="#">20</a></li>
-                                <li className="button"><a href="#">Next</a></li>
+                                <li className="button"><a href="#">Next&nbsp;
+                                    <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                                </a></li>
                             </ul>
                         </nav>
+
+                        <h4>HTML</h4>
+                        <section className="panel code">
+                        <pre>
+                        {`
+<nav role="navigation">
+    <ul className="pagination">
+        <li><a className="button" href="#">
+            <i class="fa fa-angle-double-left" aria-hidden="true">&nbsp;</i>
+            Prev</a>
+        </li>
+        <li>
+            <a className="button button-pagination selected" href="#">1</a>
+        </li>
+        <li>
+            <a className="button button-pagination" href="#">2</a>
+        </li>
+        <li>
+            <a className="button button-pagination" href="#">3</a>
+        </li>
+        <li>
+            <a className="button button-pagination" href="#">4</a>
+        </li>
+        <li><span>...</span></li>
+        <li>
+            <a className="button button-pagination" href="#">20</a>
+        </li>
+        <li className="button"><a href="#">Next&nbsp;
+            <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+        </a></li>
+    </ul>
+</nav>
+
+                        `}
+                        </pre>
+                        </section>
                     </main>
                 </div>
                 <Footer />
